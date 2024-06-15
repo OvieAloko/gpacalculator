823d759b10d730373e2fb1025c16908b6b2ed800
OvieAloko <139920388+OvieAloko@users.noreply.github.com>
2024-06-15 21:50:02 +0100

    The base of the project

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+  double size = 0;
+  double sum = 0;
+  std::cout<<"How many grades, in numbers from 1-9, do you want to add: ";
+  std::cin>>size;
+  std::cout<<size;
+
+  for (int i = 0; i<size; i++){
+    double num;
+    std::cout<<"Enter grade "<<i+1<<": ";
+    std::cin>>num;
+    sum += num;
+  }
+  double gpa = ((double)sum)/((double)size);
+
+  std::cout<<"Your GPA is "<<gpa;
+
+  system("pause");
+  return 0;
+}
